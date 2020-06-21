63f6e05455909ded39a1e300deebfd836d974432
Tal Ayalon <tal19987@gmail.com>
2020-06-21 20:33:56 +0300

    Adding the '-c' option

--- runner.py
+++ runner.py
@@ -1,8 +1,15 @@
-import argparse
+import argparse,subprocess
+
+
+def multiple_command(number, command):
+    for i in range(number):
+        subprocess.run([command])
+
 
 parser = argparse.ArgumentParser(description='Outputs a summery of execution of any command',
                                  formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-c', metavar='NUM', nargs=1, default=0, type=int,
+parser.add_argument('COMMAND', help='The command you wish to run')
+parser.add_argument('-c', metavar='NUM', type=int,
                     help='Number of times to run the given command')
 parser.add_argument('--failed-count', metavar='NUM', nargs=1, default=0, type=int,
                     help='Number of allowed failed command invocation attempts before giving up')
@@ -21,3 +28,6 @@ parser.add_argument('--debug', metavar='',
                     help='Debug mode')
 
 args = parser.parse_args()
+
+if args.c:
+    multiple_command(args.c, args.COMMAND)
