06bcf679657419011861023b55fab911f4b81a61
Tal Ayalon <tal19987@gmail.com>
2020-06-21 19:44:42 +0300

    Creating help message

---
+++ runner.py
@@ -0,0 +1,23 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Outputs a summery of execution of any command',
+                                 formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-c', metavar='NUM', nargs=1, default=0, type=int,
+                    help='Number of times to run the given command')
+parser.add_argument('--failed-count', metavar='NUM', nargs=1, default=0, type=int,
+                    help='Number of allowed failed command invocation attempts before giving up')
+parser.add_argument('--sys-trace', metavar='',
+                    help='Creates a log for:\n'
+                         '  - Disk IO\n  - Memory Usage\n  - CPU & Process info\n  - Network info'
+                    )
+parser.add_argument('--call-trace', metavar='',
+                    help='For each failed execution, add also a log with '
+                         'all the system calls ran by the command')
+
+parser.add_argument('--log-trace', metavar='',
+                    help='For each failed execution, add also the command output logs')
+
+parser.add_argument('--debug', metavar='',
+                    help='Debug mode')
+
+args = parser.parse_args()
