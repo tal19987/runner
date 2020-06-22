2e5df34b5cab06688bd9834439a94e35946de4c5
Tal Ayalon <tal19987@gmail.com>
2020-06-22 18:20:05 +0300

    Adding network logs of sys-trace

--- runner.py
+++ runner.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python3.8
 import argparse
 import subprocess
 import psutil
 from multiprocessing import Process, Queue
+
+
+def create_cpu_usage_log(file_number,data):
+    filename = 'cpu' + str(file_number) + '.log'
+    with open(filename,'w') as f:
+        f.write(data)
 
 
 def create_memory_log(file_number,data):
@@ -15,6 +20,16 @@ def create_disk_io_log(file_number,data):
     filename = 'diskio' + str(file_number) + '.log'
     with open(filename, 'w') as f:
         f.write(data)
+
+
+def get_network_usage(q):
+    data = psutil.net_io_counters()
+    q.put(str(data))
+
+
+def get_cpu_usage(q):
+    data = psutil.cpu_times()
+    q.put(str(data))
 
 
 def get_disk_io(q):
@@ -37,8 +52,8 @@ def main_command(command, number, failed, systrace):
     q1 = Queue()
     q2 = Queue()
     q3 = Queue()
-    #q4 = Queue()
-    #q5 = Queue()
+    q4 = Queue()
+    q5 = Queue()
     for i in range(number):
         if return_codes_error == failed and failed != 0:
             print(f'Command failed for {failed} times. Giving up...')
@@ -46,25 +61,27 @@ def main_command(command, number, failed, systrace):
         p1 = Process(target=run_command,args=(command,q1))
         p2 = Process(target=get_disk_io, args=(q2,))
         p3 = Process(target=get_memory,args=(q3,))
-        #p4 = Process(target=cpu_and_processes_log, args=(q4,)
-        #p5 = Process(target=network_package_log, args=(q5,))
+        p4 = Process(target=get_cpu_usage, args=(q4,))
+        p5 = Process(target=get_network_usage, args=(q5,))
+
         p1.start()
         p2.start()
         p3.start()
-        # p4.start()
-        # p5.start()
+        p4.start()
+        p5.start()
 
         p1.join()
         p2.join()
         p3.join()
-        # p4.join()
-        # p5.join()
+        p4.join()
+        p5.join()
 
+        print (q1)
         if q1.get() != 0:
             if systrace:
                 create_disk_io_log(return_codes_error,q2.get())
                 create_memory_log(return_codes_error,q3.get())
-                # create network
+                create_cpu_usage_log(return_codes_error,q4.get())
                 # create cpu
             return_codes_error += 1
 
@@ -76,7 +93,7 @@ parser.add_argument('-c', metavar='NUM', type=int, default=1,
                     help='Number of times to run the given command')
 parser.add_argument('--failed-count', type=int, default=0,
                     help='Number of allowed failed command invocation attempts before giving up')
-parser.add_argument('--sys-trace', metavar='', nargs='?', const="True",
+parser.add_argument('--sys-trace', action='store_const', metavar='', const="True",
                     help='Creates a log for:\n'
                          '  - Disk IO\n  - Memory Usage\n  - CPU & Process info\n  - Network info'
                     )
