78b99e2d48a5c3b3dd69cf693cd74c4ad095ddbb
Tal Ayalon <tal19987@gmail.com>
2020-06-23 20:46:15 +0300

    Renaming functions and adding __name__ to script

--- runner.py
+++ runner.py
@@ -15,11 +15,6 @@ def get_network_usage(q):
     q.put(str(data))
 
 
-def get_strace_otput(pid,q):
-    data = run(['strace', '-p', f"{pid}"])
-    q.put(data)
-
-
 def get_cpu_usage(q):
     data = psutil.cpu_times()
     q.put(str(data))
@@ -44,7 +39,7 @@ def run_command(command, isCallTrace, q):
     q.put(data)
 
 
-def main_command(command, number_of_runs, failed_count, systrace, calltrace, logtrace):
+def start_runner(command, number_of_runs, failed_count, systrace, calltrace, logtrace):
 
     # Defining variables
     counter_returned_error_code = 0
@@ -69,7 +64,7 @@ def main_command(command, number_of_runs, failed_count, systrace, calltrace, log
     for i in range(0, 6):
         queues[f"q{i}"] = Queue()
 
-    # Starting the main functions
+    # Starting the functions
     for i in range(number_of_runs):
         get_strace_output = ""
         processes = []
@@ -90,9 +85,10 @@ def main_command(command, number_of_runs, failed_count, systrace, calltrace, log
         for process in processes:
             process.join()
 
-        # Adds the exit code of the command to an array
+        # Adds the return object of the command to an array
         returned_objects.append(queues["q0"].get())
 
+        # Adds the exit code of the command to an array
         get_exit_codes.append(returned_objects[len(returned_objects) -1].returncode)
 
         if calltrace:
@@ -100,7 +96,7 @@ def main_command(command, number_of_runs, failed_count, systrace, calltrace, log
 
         if logtrace:
             get_log_stderr_stdout["stderr"] = returned_objects[len(returned_objects) -1].stderr.decode()
-            get_log_stderr_stdout["stdout"] = returned_objects[len(returned_objects) - 1].stdout.decode()
+            get_log_stderr_stdout["stdout"] = returned_objects[len(returned_objects) -1].stdout.decode()
 
         # Gets the last exit code of the command
         if get_exit_codes[len(get_exit_codes) -1] != 0:
@@ -115,41 +111,45 @@ def main_command(command, number_of_runs, failed_count, systrace, calltrace, log
             counter_returned_error_code += 1
     return get_exit_codes
 
-parser = argparse.ArgumentParser(description='Outputs a summery of execution of any command',
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('COMMAND', help='The command you wish to run')
-parser.add_argument('-c', metavar='NUM', type=int, default=1,
-                    help='Number of times to run the given command')
-parser.add_argument('--failed-count', type=int, default=0,
-                    help='Number of allowed failed command invocation attempts before giving up')
-parser.add_argument('--sys-trace', action='store_const', metavar='', const="True",
-                    help='Creates a log for:\n'
-                         '  - Disk IO\n  - Memory Usage\n  - CPU & Process info\n  - Network info'
-                    )
-parser.add_argument('--call-trace', action='store_const', metavar='', const="True",
-                    help='For each failed execution, add also a log with '
-                         'all the system calls ran by the command')
 
-parser.add_argument('--log-trace', action='store_const', metavar='', const="True",
-                    help='For each failed execution, add also the command output logs')
+def build_parser():
+    parser = argparse.ArgumentParser(description='Outputs a summery of execution of any command',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('COMMAND', help='The command you wish to run')
+    parser.add_argument('-c', metavar='NUM', type=int, default=1,
+                        help='Number of times to run the given command')
+    parser.add_argument('--failed-count', type=int, default=0,
+                        help='Number of allowed failed command invocation attempts before giving up')
+    parser.add_argument('--sys-trace', action='store_const', metavar='', const="True",
+                        help='Creates a log for:\n'
+                             '  - Disk IO\n  - Memory Usage\n  - CPU & Process info\n  - Network info'
+                        )
+    parser.add_argument('--call-trace', action='store_const', metavar='', const="True",
+                        help='For each failed execution, add also a log with '
+                             'all the system calls ran by the command')
 
-parser.add_argument('--debug', action='store_const', metavar='', const="True",
-                    help='Debug mode')
+    parser.add_argument('--log-trace', action='store_const', metavar='', const="True",
+                        help='For each failed execution, add also the command output logs')
 
-args = parser.parse_args()
+    parser.add_argument('--debug', action='store_const', metavar='', const="True",
+                        help='Debug mode')
+    args = parser.parse_args()
+    if args.failed_count > args.c:
+        parser.error('--failed-count value must be equal or lower than -c value (default is 1)')
 
-if args.failed_count > args.c:
-    parser.error('--failed-count value must be equal or lower than -c value (default is 1)')
-if args.call_trace and args.log_trace:
-    parser.error("--call-trace can't be used with --log-trace")
-elif args.call_trace:
-    main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace, args.call_trace, False)
-elif args.log_trace:
-    main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace, False, args.log_trace)
-exit_codes = main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace,args.call_trace, args.log_trace)
-exit_codes_fixed = Counter(exit_codes)
-number, count = Counter(exit_codes_fixed).most_common(1)[0]
-print(f"The most common exit code is- {number} which appears {count} times")
+    if args.call_trace and args.log_trace:
+        parser.error("--call-trace can't be used with --log-trace")
 
-for number , count in Counter(exit_codes_fixed).items():
-    print(f"The exit code {number} appears {count} times")
+    return args
+
+
+
+if __name__ == "__main__":
+    args = build_parser()
+    exit_codes = start_runner(args.COMMAND, args.c, args.failed_count, args.sys_trace, args.call_trace, args.log_trace)
+    exit_codes_fixed = Counter(exit_codes)
+    number, count = exit_codes_fixed.most_common(1)[0]
+    print(f"The most common exit code is- {number} which appears {count} times")
+
+    for number , count in exit_codes_fixed.items():
+        print(f"The exit code {number} appears {count} times")
