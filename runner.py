872082cc45304620236b2090559b3d0cdea3e81f
Tal Ayalon <tal19987@gmail.com>
2020-06-23 18:46:09 +0300

    Adding log-trace option and call-trace option

--- runner.py
+++ runner.py
@@ -1,8 +1,9 @@
 import argparse
-import subprocess
+from subprocess import PIPE, run
 import psutil
 from multiprocessing import Process, Queue
 from collections import Counter
+
 
 def create_logs(file_name, file_number, data):
     with open(f"{file_name}{file_number}.log",'w') as f:
@@ -12,6 +13,11 @@ def create_logs(file_name, file_number, data):
 def get_network_usage(q):
     data = psutil.net_io_counters()
     q.put(str(data))
+
+
+def get_strace_otput(pid,q):
+    data = run(['strace', '-p', f"{pid}"])
+    q.put(data)
 
 
 def get_cpu_usage(q):
@@ -29,43 +35,53 @@ def get_memory(q):
     q.put(str(data))
 
 
-def run_command(command,q):
-    data = subprocess.run(command.split())
-    q.put(data.returncode)
+def run_command(command, isCallTrace, q):
+    if isCallTrace:
+        exec_command = f"strace -c {command}"
+    else:
+        exec_command = command
+    data = run(exec_command.split(),stdout=PIPE,stderr=PIPE)
+    q.put(data)
 
 
-def main_command(command, number_of_runs, failed_count, systrace):
-    return_codes_error = 0
-    json_of_functions = {
+def main_command(command, number_of_runs, failed_count, systrace, calltrace, logtrace):
+
+    # Defining variables
+    counter_returned_error_code = 0
+    queues = {}
+    returned_objects = []
+    get_exit_codes = []
+
+    json_of_systrace_functions = {
         get_disk_io : "q1",
         get_memory : "q2",
         get_cpu_usage : "q3",
         get_network_usage : "q4"
     }
 
-    json_of_files = {
+    json_of_systrace_files = {
         "diskio" : "q1",
         "memory" : "q2",
         "cpu_usage" : "q3",
         "network" : "q4"
     }
 
-    queues = {}
-    exit_codes = []
-
-    for i in range(0, 5):
+    for i in range(0, 6):
         queues[f"q{i}"] = Queue()
 
+    # Starting the main functions
     for i in range(number_of_runs):
+        get_strace_output = ""
         processes = []
+        get_log_stderr_stdout = {}
 
-        if return_codes_error == failed_count and failed_count != 0:
+        if counter_returned_error_code == failed_count and failed_count != 0:
             print(f"The command have reached it's maximum attempts to run ({failed_count} times)")
             break
 
-        processes.append(Process(target=run_command, args=(command, queues["q0"],)))
+        processes.append(Process(target=run_command, args=(command, calltrace, queues["q0"],)))
 
-        for function_name , queue in json_of_functions.items():
+        for function_name , queue in json_of_systrace_functions.items():
             processes.append(Process(target=function_name,args=(queues[queue],)))
 
         for process in processes:
@@ -74,17 +90,30 @@ def main_command(command, number_of_runs, failed_count, systrace):
         for process in processes:
             process.join()
 
-        # Adds the exit code of the command to and array
-        exit_codes.append(queues["q0"].get())
+        # Adds the exit code of the command to an array
+        returned_objects.append(queues["q0"].get())
 
+        get_exit_codes.append(returned_objects[len(returned_objects) -1].returncode)
+
+        if calltrace:
+            get_strace_output = str(returned_objects[len(returned_objects) -1].stderr.decode())
+
+        if logtrace:
+            get_log_stderr_stdout["stderr"] = returned_objects[len(returned_objects) -1].stderr.decode()
+            get_log_stderr_stdout["stdout"] = returned_objects[len(returned_objects) - 1].stdout.decode()
 
         # Gets the last exit code of the command
-        if exit_codes[len(exit_codes) -1] != 0:
+        if get_exit_codes[len(get_exit_codes) -1] != 0:
             if systrace:
-                for file , queue in json_of_files.items():
-                    create_logs(file, return_codes_error, queues[queue].get())
-            return_codes_error += 1
-    return exit_codes
+                for file , queue in json_of_systrace_files.items():
+                    create_logs(file, counter_returned_error_code, queues[queue].get())
+            if calltrace:
+                create_logs("calltrace", counter_returned_error_code, get_strace_output)
+            if logtrace:
+                stderr , stdout = get_log_stderr_stdout["stderr"] , get_log_stderr_stdout["stdout"]
+                create_logs("logtrace", counter_returned_error_code, f"stdout is: {stdout}\nstderr is: {stderr}")
+        counter_returned_error_code += 1
+    return get_exit_codes
 
 parser = argparse.ArgumentParser(description='Outputs a summery of execution of any command',
                                  formatter_class=argparse.RawTextHelpFormatter)
@@ -110,9 +139,14 @@ parser.add_argument('--debug', action='store_const', metavar='', const="True",
 args = parser.parse_args()
 
 if args.failed_count > args.c:
-    parser.error('--failed-count value must be equal or higher than -c value (default is 1)')
-
-exit_codes = main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace)
+    parser.error('--failed-count value must be equal or lower than -c value (default is 1)')
+if args.call_trace and args.log_trace:
+    parser.error("--call-trace can't be used with --log-trace")
+elif args.call_trace:
+    main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace, args.call_trace, False)
+elif args.log_trace:
+    main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace, False, args.log_trace)
+exit_codes = main_command(args.COMMAND, args.c, args.failed_count, args.sys_trace,args.call_trace, args.log_trace)
 exit_codes_fixed = Counter(exit_codes)
 number, count = Counter(exit_codes_fixed).most_common(1)[0]
 print(f"The most common exit code is- {number} which appears {count} times")
